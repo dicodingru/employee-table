4e092fa2ff877deb0dcca9faacb06bbb0166f974
Di Coding <dicoding@yandex.ru>
2018-08-18 14:30:02 +0300

    Use HMR

--- src/index.js
+++ src/index.js
@@ -6,3 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if (module.hot) {
+  module.hot.accept();
+}
